d00650f731b97c4d747ffc1efdecf243e60d2895
daydy225 <dabelydadi@gmail.com>
2021-09-30 11:33:11 +0000

    Save 4

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const input = document.getElementById("convert").value
-const result = document.getElementById("result")
+let input = document.getElementById("convert").value
+let result = document.getElementById("result")
 
 
 
@@ -9,8 +9,9 @@ function convertir() {
 //  let mtn = ["04", "05", "06", "44", "45", "46", "54", "55", "56", "64", "65", "66", "74", "75", "76", "84", "85", "86", "94", "95", "96"];
 //  let moov = ["01", "02", "03", "41", "42", "43", "51", "52", "53", "61", "62", "63", "71", "72", "73", "81", "82", "83", "91", "92", "93"];
  let firstNum = input[0]+input[1];
-if (input.length === 8) {
-  let text  
+ let text 
+if (input.length === 8 ) {
+   
   switch(firstNum) {
      case "01":
      case "02":
@@ -20,15 +21,15 @@ if (input.length === 8) {
      case "04":
      case "05":
      case "06":
-         text = "Votre numero a 10 chiffres est : 05" + input;
+       text = "Votre numero a 10 chiffres est : 05" + input;
      break;
      case "07": 
      case "08": 
      case "09":
-         text = "Votre numero a 10 chiffres est : 07" + input;
+       text = "Votre numero a 10 chiffres est : 07" + input;
      break;
      default:
-     text = "Le numero n'est pas pris en compte dans la convertion";
+       text = "Le numero n'est pas pris en compte dans la convertion";
    }
    result.innerHTML = text;
 
